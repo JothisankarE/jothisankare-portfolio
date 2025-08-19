1c3083d0cef73bcf13c75d21ae01d2e770e18d5b
JOTHISANKAR E <jothisankar979@gmail.com>
2025-08-19 10:50:04 +0530

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -60,27 +60,28 @@ const Hero = () => {
             about new and emerging trends in the IT sector.
           </p>
           <div className="flex flex-col sm:flex-row gap-4 justify-center items-center mb-8">
-  <a
-    href="https://drive.google.com/file/d/10hNATligox4vCLZJ3LGCtfVQu9C9pMD7/view?usp=sharing"
-    className="w-full sm:w-auto"
-  >
-    <Button size="lg" className="glow-hover w-full sm:w-auto">
-      <Download className="mr-2 h-4 w-4" />
-      Download Resume
-    </Button>
-  </a>
-</div>
+            <a
+              href="https://drive.google.com/file/d/10hNATligox4vCLZJ3LGCtfVQu9C9pMD7/view?usp=sharing"
+              className="w-full sm:w-auto"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Button size="lg" className="glow-hover w-full sm:w-auto">
+                <Download className="mr-2 h-4 w-4" />
+                Download Resume
+              </Button>
+            </a>
+          </div>
 
-            <div className="flex gap-4">
-              <Button variant="outline" size="lg" className="glow-hover">
-                <Mail className="mr-2 h-4 w-4" />
-                jothisankar979@gmail.com
-              </Button>
-              <Button variant="outline" size="lg" className="glow-hover">
-                <Phone className="mr-2 h-4 w-4" />
-                +91 9994634216
-              </Button>
-            </div>
+          <div className="flex gap-4">
+            <Button variant="outline" size="lg" className="glow-hover">
+              <Mail className="mr-2 h-4 w-4" />
+              jothisankar979@gmail.com
+            </Button>
+            <Button variant="outline" size="lg" className="glow-hover">
+              <Phone className="mr-2 h-4 w-4" />
+              +91 9994634216
+            </Button>
           </div>
         </div>
       </div>
